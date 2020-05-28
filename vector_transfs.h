487c686f2d7b0df1f7dc555444d54658e89d961d
Serhii Yatsenko <royalroad1995@gmail.com>
2020-05-28 12:48:01 +0300

    Upfate file
    Fixed TABs.

--- vector_transfs.h
+++ vector_transfs.h
@@ -5,8 +5,8 @@
   * @version V1.0
   * @date    May-2020
   * @brief   This file contains the type definitions of data structures and function
-  *			 		 prototypes for implementation the various types of Clarke and Park
-  *			 		 transformations.
+  *	     prototypes for implementation the various types of Clarke and Park
+  *	     transformations.
   ***********************************************************************************
   * @license
   *
@@ -38,7 +38,7 @@
 #define __VECTOR_TRANSFS_H__
 
 #ifdef __cplusplus
-	extern "C" {
+extern "C" {
 #endif
 
 /* Includes -----------------------------------------------------------------------*/
@@ -47,179 +47,179 @@
 /* Exported types -----------------------------------------------------------------*/
 
 /** 
-  * @brief	"Forward full Clarke transformation Module" data structure: ABC to Al-Be
+  * @brief "Forward full Clarke transformation Module" data structure: ABC to Al-Be
   */ 
 typedef struct sFFClarke
 {
 // Inputs:
-	float fA;																// A input
-	float fB;																// B input
-	float fC;																// C input
+	float fA;				// A input
+	float fB;				// B input
+	float fC;				// C input
 // Outputs:
-	float fAl;															// Alpha output
-	float fBe;															// Beta output
+	float fAl;				// Alpha output
+	float fBe;				// Beta output
 // Functions:
 	void  (*m_abc2albe)(struct sFFClarke*);	// pointer to calculation function
 } tFFClarke;
 
 /** 
-  * @brief	"Forward reduce Clarke transformation Module" data structure: AB to Al-Be
+  * @brief "Forward reduce Clarke transformation Module" data structure: AB to Al-Be
   */ 
 typedef struct sFRClarke
 {
 // Inputs:
-	float fA;																// A input
-	float fB;																// B input
+	float fA;				// A input
+	float fB;				// B input
 // Outputs:
-	float fAl;															// Alpha output
-	float fBe;															// Beta output
+	float fAl;				// Alpha output
+	float fBe;				// Beta output
 // Functions:
 	void  (*m_ab2albe)(struct sFRClarke*);	// pointer to calculation function
 } tFRClarke;
 
 /** 
-  * @brief	"Inverse full Clarke transformation Module" data structure: Al-Be to ABC
+  * @brief "Inverse full Clarke transformation Module" data structure: Al-Be to ABC
   */ 
 typedef struct sIFClarke
 {
 // Inputs:
-	float fAl;															// Alpha input
-	float fBe;															// Beta input
+	float fAl;				// Alpha input
+	float fBe;				// Beta input
 // Outputs:
-	float fA;																// A output
-	float fB;																// B output
-	float fC;																// C output
+	float fA;				// A output
+	float fB;				// B output
+	float fC;				// C output
 // Functions:
 	void  (*m_albe2abc)(struct sIFClarke*);	// pointer to calculation function
 } tIFClarke;
 
 /** 
-  * @brief	"Inverse reduce Clarke transformation Module" data structure: Al-Be to AB
+  * @brief "Inverse reduce Clarke transformation Module" data structure: Al-Be to AB
   */ 
 typedef struct sIRClarke
 {
 // Inputs:
-	float fAl;															// Alpha input
-	float fBe;															// Beta input
+	float fAl;				// Alpha input
+	float fBe;				// Beta input
 // Outputs:
-	float fA;																// A output
-	float fB;																// B output
+	float fA;				// A output
+	float fB;				// B output
 // Functions:
 	void  (*m_albe2ab)(struct sIRClarke*);	// pointer to calculation function
 } tIRClarke;
 
 /** 
-  * @brief	"Forward Park transformation Module" data structure: Al-Be to DQ
+  * @brief "Forward Park transformation Module" data structure: Al-Be to DQ
   */ 
 typedef struct sFPark
 {
 // Inputs:
-	float fAl;															// Alpha input
-	float fBe;															// Beta input
-	float fSinAng;													// sin(Angle) input
-	float fCosAng;													// cos(Angle) input
+	float fAl;				// Alpha input
+	float fBe;				// Beta input
+	float fSinAng;				// sin(Angle) input
+	float fCosAng;				// cos(Angle) input
 // Outputs:
-	float fD;																// D output
-	float fQ;																// Q output
+	float fD;				// D output
+	float fQ;				// Q output
 // Functions:
-	void  (*m_albe2dq)(struct sFPark*);			// pointer to calculation function
+	void  (*m_albe2dq)(struct sFPark*);	// pointer to calculation function
 } tFPark;
 
 /** 
-  * @brief	"Inverse Park transformation Module" data structure: DQ to Al-Be
+  * @brief "Inverse Park transformation Module" data structure: DQ to Al-Be
   */ 
 typedef struct sIPark
 {
 // Inputs:
-	float fD;																// D input
-	float fQ;																// Q input
-	float fSinAng;													// sin(Angle) input
-	float fCosAng;													// cos(Angle) input
+	float fD;				// D input
+	float fQ;				// Q input
+	float fSinAng;				// sin(Angle) input
+	float fCosAng;				// cos(Angle) input
 // Outputs:
-	float fAl;															// Alpha output
-	float fBe;															// Beta output
+	float fAl;				// Alpha output
+	float fBe;				// Beta output
 // Functions:
-	void  (*m_dq2albe)(struct sIPark*);			// pointer to calculation function
+	void  (*m_dq2albe)(struct sIPark*);	// pointer to calculation function
 } tIPark;
 
 /* Exported constants -------------------------------------------------------------*/
 
 /** 
-  * @brief	Initialization constant with defaults for user variables
-  *			with "tFFClarke" type
+  * @brief Initialization constant with defaults for user variables
+  *	   with "tFFClarke" type
   */
-#define FF_CLARKE_DEFAULTS {							\
-	.fA					= 0.0f,											\
-	.fB					= 0.0f,											\
-	.fC					= 0.0f,											\
-	.fAl				= 0.0f,											\
-	.fBe				= 0.0f,											\
-	.m_abc2albe	= tFFClarke_abc2albe				\
+#define FF_CLARKE_DEFAULTS {			\
+	.fA		= 0.0f,			\
+	.fB		= 0.0f,			\
+	.fC		= 0.0f,			\
+	.fAl		= 0.0f,			\
+	.fBe		= 0.0f,			\
+	.m_abc2albe	= tFFClarke_abc2albe	\
 }
 
 /** 
-  * @brief	Initialization constant with defaults for user variables
-  *			with "tFRClarke" type
+  * @brief Initialization constant with defaults for user variables
+  *	   with "tFRClarke" type
   */
-#define FR_CLARKE_DEFAULTS {							\
-	.fA					= 0.0f,											\
-	.fB					= 0.0f,											\
-	.fAl				= 0.0f,											\
-	.fBe				= 0.0f,											\
-	.m_ab2albe	= tFRClarke_ab2albe					\
+#define FR_CLARKE_DEFAULTS {			\
+	.fA		= 0.0f,			\
+	.fB		= 0.0f,			\
+	.fAl		= 0.0f,			\
+	.fBe		= 0.0f,			\
+	.m_ab2albe	= tFRClarke_ab2albe	\
 }
 
 /** 
-  * @brief	Initialization constant with defaults for user variables
-  *			with "tIFClarke" type
+  * @brief Initialization constant with defaults for user variables
+  *	   with "tIFClarke" type
   */
-#define IF_CLARKE_DEFAULTS {							\
-	.fAl				= 0.0f,											\
-	.fBe				= 0.0f,											\
-	.fA					= 0.0f,											\
-	.fB					= 0.0f,											\
-	.fC					= 0.0f,											\
-	.m_albe2abc	= tIFClarke_albe2abc				\
+#define IF_CLARKE_DEFAULTS {			\
+	.fAl		= 0.0f,			\
+	.fBe		= 0.0f,			\
+	.fA		= 0.0f,			\
+	.fB		= 0.0f,			\
+	.fC		= 0.0f,			\
+	.m_albe2abc	= tIFClarke_albe2abc	\
 }
 
 /** 
-  * @brief	Initialization constant with defaults for user variables
-  *			with "tIRClarke" type
+  * @brief Initialization constant with defaults for user variables
+  *	   with "tIRClarke" type
   */
-#define IR_CLARKE_DEFAULTS {							\
-	.fAl				= 0.0f,											\
-	.fBe				= 0.0f,											\
-	.fA					= 0.0f,											\
-	.fB					= 0.0f,											\
-	.m_albe2ab	= tIRClarke_albe2ab					\
+#define IR_CLARKE_DEFAULTS {			\
+	.fAl		= 0.0f,			\
+	.fBe		= 0.0f,			\
+	.fA		= 0.0f,			\
+	.fB		= 0.0f,			\
+	.m_albe2ab	= tIRClarke_albe2ab	\
 }
 
 /** 
-  * @brief	Initialization constant with defaults for user variables
-  *			with "tFPark" type
+  * @brief Initialization constant with defaults for user variables
+  *	   with "tFPark" type
   */
-#define F_PARK_DEFAULTS {									\
-	.fAl				= 0.0f,											\
-	.fBe				= 0.0f,											\
-	.fD					= 0.0f,											\
-	.fQ					= 0.0f,											\
-	.fSinAng		= 0.0f,											\
-	.fCosAng		= 0.0f,											\
-	.m_albe2dq	= tFPark_albe2dq						\
+#define F_PARK_DEFAULTS {			\
+	.fAl		= 0.0f,			\
+	.fBe		= 0.0f,			\
+	.fD		= 0.0f,			\
+	.fQ		= 0.0f,			\
+	.fSinAng	= 0.0f,			\
+	.fCosAng	= 0.0f,			\
+	.m_albe2dq	= tFPark_albe2dq	\
 }
 
 /** 
-  * @brief	Initialization constant with defaults for user variables
-  *			with "tIPark" type
+  * @brief Initialization constant with defaults for user variables
+  *	   with "tIPark" type
   */
-#define I_PARK_DEFAULTS {									\
-	.fD					= 0.0f,											\
-	.fQ					= 0.0f,											\
-	.fAl				= 0.0f,											\
-	.fBe				= 0.0f,											\
-	.fSinAng		= 0.0f,											\
-	.fCosAng		= 0.0f,											\
-	.m_dq2albe	= tIPark_dq2albe						\
+#define I_PARK_DEFAULTS {			\
+	.fD		= 0.0f,			\
+	.fQ		= 0.0f,			\
+	.fAl		= 0.0f,			\
+	.fBe		= 0.0f,			\
+	.fSinAng	= 0.0f,			\
+	.fCosAng	= 0.0f,			\
+	.m_dq2albe	= tIPark_dq2albe	\
 }
 	
 /* Exported macro -----------------------------------------------------------------*/
